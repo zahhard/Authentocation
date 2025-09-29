aa0e24991c104f1dc7db5a838fb90f12c062ac66
Zahara <za.davardoust@gmail.com>
2025-09-30 00:34:22 +0330

    middleware added to handle private route

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const token = request.cookies.get("token")?.value
+
+    if (token) {
+        return NextResponse.next();
+    }
+
+    const url = new URL(request.url)
+    url.pathname = "/login"
+
+    return NextResponse.redirect(url.toString())
+
+
+}
+
+export const config = {
+    matcher: ["/dashboard/:path*"]
+}
